62c7a1fc704637ebb8f9af4a58a3e97520eb3190
onias <salvatore.romano.3@studenti.unipd.it>
2021-08-19 00:30:19 +0200

    fix import pag4

--- react-pieces/src/questions/QuestionPage4.js
+++ react-pieces/src/questions/QuestionPage4.js
@@ -1,15 +1,14 @@
 import _ from 'lodash';
-
 import React from 'react';
-import { makeStyles} from '@material-ui/core/styles';
+import {withStyles, makeStyles} from '@material-ui/core/styles';
 
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
+
 import RadioGroup from '@material-ui/core/RadioGroup';
 import Radio from '@material-ui/core/Radio'
 import TextField from '@material-ui/core/TextField';
 import Slider from '@material-ui/core/Slider';
-
 
 import Button from '@material-ui/core/Button';
 
@@ -74,6 +73,35 @@ const marks3 = [
   },
 ];
 
+const PrettoSlider = withStyles({
+  root: {
+    color: '#e33180',
+    height: 20,
+  },
+  thumb: {
+    height: 24,
+    width: 24,
+    backgroundColor: '#fff',
+    border: '2px solid currentColor',
+    marginTop: -8,
+    marginLeft: -12,
+    '&:focus, &:hover, &$active': {
+      boxShadow: 'inherit',
+    },
+  },
+  active: {},
+  valueLabel: {
+    left: 'calc(-50% + 4px)',
+  },
+  track: {
+    height: 8,
+    borderRadius: 4,
+  },
+  rail: {
+    height: 8,
+    borderRadius: 4,
+  },
+})(Slider);
 
 export default function QuestionPage4() {
 
