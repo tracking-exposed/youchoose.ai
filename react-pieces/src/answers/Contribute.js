88f579e540f2b2c974d72787e269da73dfa26e9f
Claudio <claudio@syrianarchive.org>
2021-08-19 15:49:03 +0200

    progress etc

--- react-pieces/src/answers/Contribute.js
+++ react-pieces/src/answers/Contribute.js
@@ -15,7 +15,12 @@ class Contribute extends React.Component{
   }
 
   render () {
+    const kind = this.props.kind;
     const rows = this.props.data;
+    const keys = _.uniq(_.flatten(_.map(rows, _.keys)));
+
+    console.log(kind, rows, keys);
+
     return (
       <TableContainer component={Paper}>
         <Table stickyHeader aria-label="simple table">
@@ -26,29 +31,6 @@ class Contribute extends React.Component{
               <TableCell align="right">lastUpdate</TableCell>
               <TableCell align="right">version</TableCell>
               <TableCell align="right">11</TableCell>
-              <TableCell align="right">12</TableCell>
-              <TableCell align="right">13</TableCell>
-              <TableCell align="right">14</TableCell>
-              <TableCell align="right">15</TableCell>
-              <TableCell align="right">16</TableCell>
-              <TableCell align="right">17</TableCell>
-              <TableCell align="right">18</TableCell>
-              <TableCell align="right">19</TableCell>
-              <TableCell align="right">21</TableCell>
-              <TableCell align="right">22</TableCell>
-              <TableCell align="right">23</TableCell>
-              <TableCell align="right">24</TableCell>
-              <TableCell align="right">25</TableCell>
-              <TableCell align="right">26</TableCell>
-              <TableCell align="right">27</TableCell>
-              <TableCell align="right">31</TableCell>
-              <TableCell align="right">32</TableCell>
-              <TableCell align="right">33</TableCell>
-              <TableCell align="right">34</TableCell>
-              <TableCell align="right">35</TableCell>
-              <TableCell align="right">36</TableCell>
-              <TableCell align="right">41</TableCell>
-              <TableCell align="right">42</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -59,29 +41,6 @@ class Contribute extends React.Component{
                 <TableCell align="right" alt={row['lastUpdate']}>{row['timeago']}</TableCell>
                 <TableCell align="right">{row['version']}</TableCell>
                 <TableCell align="right">{row['11']}</TableCell>
-                <TableCell align="right">{row['12']}</TableCell>
-                <TableCell align="right">{row['13']}</TableCell>
-                <TableCell align="right">{row['14']}</TableCell>
-                <TableCell align="right">{row['15']}</TableCell>
-                <TableCell align="right">{row['16']}</TableCell>
-                <TableCell align="right">{row['17']}</TableCell>
-                <TableCell align="right">{row['18']}</TableCell>
-                <TableCell align="right">{row['19']}</TableCell>
-                <TableCell align="right">{row['21']}</TableCell>
-                <TableCell align="right">{row['22']}</TableCell>
-                <TableCell align="right">{row['23']}</TableCell>
-                <TableCell align="right">{row['24']}</TableCell>
-                <TableCell align="right">{row['25']}</TableCell>
-                <TableCell align="right">{row['26']}</TableCell>
-                <TableCell align="right">{row['27']}</TableCell>
-                <TableCell align="right">{row['31']}</TableCell>
-                <TableCell align="right">{row['32']}</TableCell>
-                <TableCell align="right">{row['33']}</TableCell>
-                <TableCell align="right">{row['34']}</TableCell>
-                <TableCell align="right">{row['35']}</TableCell>
-                <TableCell align="right">{row['36']}</TableCell>
-                <TableCell align="right">{row['41']}</TableCell>
-                <TableCell align="right">{row['42']}</TableCell>
               </TableRow>
             ))}
           </TableBody>
